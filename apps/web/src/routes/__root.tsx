527cfab49fcc1fe210a2af924c8a70af46f88404
jsefiani <jawad.sefiani@hotmail.com>
2026-02-14 09:31:03 +0100

    load fonts in root instead of tailwind

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -3,10 +3,6 @@ import {
   Scripts,
   createRootRouteWithContext,
 } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
-
-import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
 
 import appCss from '../styles.css?url'
 
@@ -32,6 +28,19 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
     ],
     links: [
       {
+        rel: 'preconnect',
+        href: 'https://fonts.googleapis.com',
+      },
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.gstatic.com',
+        crossOrigin: 'anonymous',
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400&display=swap',
+      },
+      {
         rel: 'stylesheet',
         href: appCss,
       },
@@ -49,18 +58,6 @@ function RootDocument({ children }: { children: React.ReactNode }) {
       </head>
       <body className="bg-zinc-950">
         {children}
-        {/* <TanStackDevtools
-          config={{
-            position: 'bottom-right',
-          }}
-          plugins={[
-            {
-              name: 'Tanstack Router',
-              render: <TanStackRouterDevtoolsPanel />,
-            },
-            TanStackQueryDevtools,
-          ]}
-        /> */}
         <Scripts />
       </body>
     </html>
